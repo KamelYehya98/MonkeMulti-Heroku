7ec494ef5d2610a0142181ab4b7246220a96aa91
Kamel Yehya <kamel.yehya04@gmail.com>
2021-09-05 01:20:58 +0300

    Update Test.js

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Test() {
 
-    let data = undefined;
-    try{
-        console.log('Reacccccccccccccccched getting GET requests');
-        const res = await fetch('*', {
-            method: 'GET',
-            headers: { 'Content-Type' : 'application/json' },
-            credentials: 'include'
-        });
-        data = await res.json();
-        console.log(data);
-    }catch(err){
-        console.log(err);
+    const [data, setData] = useState(undefined);
+    
+    async function khara(){
+        try{
+            console.log('Reacccccccccccccccched getting GET requests');
+            const res = await fetch('*', {
+                method: 'GET',
+                headers: { 'Content-Type' : 'application/json' },
+                credentials: 'include'
+            });
+            setData( await res.json() );
+            console.log(data);
+        }catch(err){
+            console.log(err);
+        }
     }
-
-    if(data.user){
-        return(
-            <h1>User exists</h1>
-        );
+    khara();
+    if(data){
+        if(data.user){
+            return(
+                <h1>User exists</h1>
+            );
+        }else{
+            return(
+                <h1>User doesn't exist</h1>
+            );
+        }
     }else{
         return(
-            <h1>User doesn't exist</h1>
+            <h1>Data undefined</h1>
         );
     }
 }
