c477cc1b09ca9d88fb44fe2a46bae65071d04293
Kamel Yehya <kamel.yehya04@gmail.com>
2021-09-05 00:46:49 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //import { set } from "mongoose";
 
 
-function App() {
+async function App() {
 
     // Global state variables
     const socket = io();
