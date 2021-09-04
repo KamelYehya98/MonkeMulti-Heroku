f8134ca36a2befb841709b17957c814957996b02
Kamel Yehya <kamel.yehya04@gmail.com>
2021-09-04 19:26:52 +0300

    player-stats-table(3)

--- client/src/components/PlayerStats.js
+++ client/src/components/PlayerStats.js
@@ -2,34 +2,38 @@ import React from 'react';
 import 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default async function PlayerStats({username}) {
+export default function PlayerStats({ username }) {
 
     let gamesPlayed = '-';
     let roundsPlayed = '-';
     let winrate = '-';
     let rating = '-';
 
-    try{
-        console.log('Reacccccccccccccccched getting stats');
-        const res = await fetch('/getstats', {
-            method: 'POST',
-            body: JSON.stringify({ username }),
-            headers: { 'Content-Type' : 'application/json' },
-            credentials: 'include'
-        });
-        const data = await res.json();
-        let err_text = document.getElementById('stats-container');
-        if(data.error){
-            err_text.innerHTML = "Your session ended - Please login again";
-        }else{
-            gamesPlayed = data.gamesPlayed;
-            roundsPlayed = data.roundsPlayed;
-            winrate = data.winrate;
-            rating = data.rating;
+    async function getStats(){
+        try{
+            console.log('Reacccccccccccccccched getting stats');
+            const res = await fetch('/getstats', {
+                method: 'POST',
+                body: JSON.stringify({ username }),
+                headers: { 'Content-Type' : 'application/json' },
+                credentials: 'include'
+            });
+            const data = await res.json();
+            let err_text = document.getElementById('stats-container');
+            if(data.error){
+                err_text.innerHTML = "Your session ended - Please login again";
+            }else{
+                gamesPlayed = data.gamesPlayed;
+                roundsPlayed = data.roundsPlayed;
+                winrate = data.winrate;
+                rating = data.rating;
+            }
+        }catch(err){
+            console.log(err);
         }
-    }catch(err){
-        console.log(err);
     }
+    
+    getStats();
     
     return (
         <div id='stats-container' className="text-danger w-100">
