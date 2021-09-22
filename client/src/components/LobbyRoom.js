6a9859ce03f8500b18d14e2bc152970765dece3f
ImadAbouHatab <1imadabouhatab@gmail.com>
2021-09-23 01:25:17 +0300

    Dot dot dot

--- client/src/components/LobbyRoom.js
+++ client/src/components/LobbyRoom.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import sok from "../services/socket";
 import { useHistory } from "react-router-dom";
+import "./dots.css";
 
 export default function LobbyRoom({roomID}) {
     var socket = sok.getSocket();
@@ -17,6 +18,17 @@ export default function LobbyRoom({roomID}) {
     });
     console.log(`Room ID at lobby room ${roomID}`);
     return(
-        <div><h1>Waiting for Opponent...</h1><br/><p>Room ID: {roomID}</p></div>
+        <div className="mt-5">
+            <div className="d-flex align-items-baseline justify-content-center pt-5">
+                <h1>Waiting for Opponent</h1>
+                <div class="snippet" data-title=".dot-typing">
+                    <div class="stage">
+                    <div class="dot-typing"></div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <center><p>Room ID: {roomID}</p></center>
+        </div>
     );
 }
